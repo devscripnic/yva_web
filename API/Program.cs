f2c55819a29c92acd0df0e7f9c19fe1886678913
devscripnic <devscripnic@gmail.com>
2026-03-26 10:31:56 +0300

    add swagger

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Register controller support
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "YVA API v1");
+        c.RoutePrefix = "swagger";
+    });
+}
 
 // Connecting controller routing
 app.MapControllers();
